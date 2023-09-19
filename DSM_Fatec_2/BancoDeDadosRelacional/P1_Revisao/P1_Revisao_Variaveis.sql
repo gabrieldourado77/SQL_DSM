c04b8b30027c3a1b1a164feb7741a3e1e94c99a4
Gabriel Dourado dos Santos <82122849+gabrieldourado77@users.noreply.github.com>
2023-09-19 19:10:15 -0300

    Delete DSM_Fatec_2/BancoDeDadosRelacional/P1_Revisao directory

--- DSM_Fatec_2/BancoDeDadosRelacional/P1_Revisao/P1_Revisao_Variaveis.sql
+++
@@ -1,8 +0,0 @@
--- Revisão para a P1 - DDL (Data Definition Language)
--- Variáveis
-
--- Criar uma variável
-DECLARE @nome_variavel varchar(50);
-
--- Atribuir um valor á váriavel
-SET @nome_variavel = ' ';
--- DSM_Fatec_2/BancoDeDadosRelacional/P1_Revisao/README.md
+++
@@ -1,2 +0,0 @@
-# Revisão para a P1
-Resumos dos conteúdos ensinados, até a data da P1, com explicações e exemplos.
